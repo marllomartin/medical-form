54e8dd13ad4cd57482c3a3fef229d42652e626ce
marllomartin <marllon3636@gmail.com>
2022-09-10 18:24:46 -0300

    feat(src/components): cria styled component Header

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export default function Header() {
+  return (
+    <Container>
+      <h2>Header</h2>
+    </Container>
+  )
+}
---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100vw;
+  height: 50px;
+  padding: 0px 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  & h2 {
+    font-size: 35px;
+    text-align: center;
+    color: #000;
+  }
+`;
