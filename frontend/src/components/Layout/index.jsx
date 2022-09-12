74736634f0d0b47ab527ddb7e16add487d54a4e0
marllomartin <marllon3636@gmail.com>
2022-09-12 20:02:02 -0300

    feat(src/components): adiciona barra de pesquisa no layout

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header'
 import DoctorForm from '../DoctorForm'
+import DoctorSearch from '../DoctorSearch'
 import DoctorsTable from '../DoctorsTable'
 
 import { Container } from './styles'
@@ -11,6 +12,7 @@ export default function Layout() {
       <Container>
         <Header />
         <DoctorForm />
+        <DoctorSearch />
         <DoctorsTable />
       </Container>
     </>
