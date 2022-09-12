496f2899a03c5993411cc28e763034dc2019dfd0
marllomartin <marllon3636@gmail.com>
2022-09-12 19:59:32 -0300

    refactor(src/hooks): refatora hook useAxios

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -2,11 +2,15 @@ import useSWR from "swr";
 import api from "../services/api";
 
 export function useAxios(url) {
-  const { data, error, mutate } = useSWR(url, async (url) => {
-    const { data } = await api.get(url);
+  const { data, error, mutate } = useSWR(
+    url,
+    async (url) => {
+      const response = await api.get(url);
 
-    return data;
-  });
+      return response.data;
+    },
+    {}
+  );
 
   return { data, error, mutate };
 }
