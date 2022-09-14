1f77cc78d289aad4da81d10f2c089fda0b5344a0
marllomartin <marllon3636@gmail.com>
2022-09-13 21:05:04 -0300

    feat(src): adiciona novas rotas ao servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,14 +6,20 @@ const {
   createDoctor,
   updateDoctor,
   deleteDoctor
-} = require('./controllers/doctor')
-const validateDoctor = require('./middlewares/validateDoctor')
+} = require('./controllers/doctor');
+const validateDoctor = require('./middlewares/validateDoctor');
+
+const { findAllExpertises } = require('./controllers/expertise');
 
 const port = process.env.PORT;
 
+// Doctors
 app.get('/doctors', findAllDoctors);
 app.post('/doctors', validateDoctor, createDoctor);
 app.patch('/doctors/:id', updateDoctor);
 app.delete('/doctors/:id', deleteDoctor);
+
+// Expertises
+app.get('/expertises', findAllExpertises);
 
 app.listen(port, () => console.log('Ouvindo porta', port));
