9e4fc01fa9e19c57454b4d99f1303d2ce98db1ce
marllomartin <marllon3636@gmail.com>
2022-09-14 16:34:26 -0300

    fix(src/services): ajusta serviço de excluir médicos

--- backend/src/services/doctor.js
+++ backend/src/services/doctor.js
@@ -13,6 +13,7 @@ const findAllDoctors = async () => {
 const createDoctor = async (data) => {
   const { name, uf, crm, phone, expertises } = data;
   const doctorExists = await doctor.findOne({ where: { crm } });
+
   if (doctorExists) throw new Error('Médico já registrado');
 
   const newDoctor = await doctor.create({
@@ -22,9 +23,9 @@ const createDoctor = async (data) => {
     phone
   });
 
-  const newDoctorExpertise = await expertises.map((x) => ({
+  const newDoctorExpertise = await expertises.map((expertise) => ({
     doctorId: newDoctor.id,
-    expertiseId: x.id
+    expertiseId: expertise.id
   }))
 
   await doctorExpertise.bulkCreate(newDoctorExpertise)
@@ -33,16 +34,35 @@ const createDoctor = async (data) => {
 };
 
 const updateDoctor = async (id, data) => {
+  const { name, uf, crm, phone, expertises } = data;
   const doctorExists = await doctor.findByPk(id);
+
   if (!doctorExists) throw new Error('Médico não encontrado');
 
-  const updatedDoctor = await doctor.update(data, { where: { id } });
+  const updatedDoctor = await doctor.update({
+    name,
+    uf,
+    crm,
+    phone
+  }, { where: { id } });
+
+  const deleteDoctorExpertise = doctorExpertise.destroy({ where: { doctorId: id } });
+
+  const updatedExpertise = expertises.map((expertise) => ({
+    doctorId: id,
+    expertiseId: expertise.id
+  }))
+
+  const updateDoctorExpertise = doctorExpertise.bulkCreate(updatedExpertise)
+
+  await Promise.all([deleteDoctorExpertise, updatedExpertise, updateDoctorExpertise])
 
   return updatedDoctor;
 };
 
 const deleteDoctor = async (id) => {
   const doctorExists = await doctor.findByPk(id);
+
   if (!doctorExists) throw new Error('Médico não encontrado');
 
   await doctor.destroy({ where: { id } });
