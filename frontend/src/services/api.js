3989ec047497ae4a97c3860253bc21deb0d88867
Marllon <marllon3636@gmail.com>
2022-09-21 18:03:54 -0300

    style(src/services): fix lint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,20 +6,20 @@ export const api = axios.create({
 
 export const getDoctors = async () => {
   return api.get("doctors");
-}
+};
 
 export const getExpertises = async () => {
   return api.get("expertises");
-}
+};
 
 export const createDoctor = async (name, uf, crm, phone, expertises) => {
   return api.post("doctors", { name, uf, crm, phone, expertises });
-}
+};
 
 export const updateDoctor = async (id, name, uf, crm, phone, expertises) => {
   return api.patch(`doctors/${id}`, { name, uf, crm, phone, expertises });
-}
+};
 
 export const deleteDoctor = async (id) => {
   return api.delete(`doctors/${id}`);
-}
+};
